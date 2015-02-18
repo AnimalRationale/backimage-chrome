9a7134fd13aae86db3e3f1548379b60484bb25f2
AnimalRationale <wojciech.mardyla@gmail.com>
2015-02-18 23:57:36 +0100

    Scaffold for change command.

---
+++ background.js
@@ -0,0 +1,9 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
+    chrome.tabs.sendMessage(tab.id, {
+            command: "change_background"
+
+        },
+        function(msg) {
+            console.log("Response:", msg);
+        });
+});
