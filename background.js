8c2b488123e6c99bb009caa0ebde9054ea785d71
AnimalRationale <wojciech.mardyla@gmail.com>
2015-02-19 03:18:53 +0100

    Sanity cleanup :).

--- background.js
+++ background.js
@@ -1,7 +1,6 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.sendMessage(tab.id, {
             command: "change_background"
-
         },
         function(msg) {
             console.log("Response:", msg);
