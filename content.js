3e9e26457a4ad384bad70d7c2fa7161bce55bc7e
AnimalRationale <wojciech.mardyla@gmail.com>
2015-02-22 22:23:35 +0100

    Cleanup.

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
     if (msg.command && (msg.command == "change_background")) {
-        document.body.style.backgroundImage = "url('https://img.4plebs.org/boards/hr/image/1374/42/1374429792284.jpg')";
+        document.body.style.backgroundImage = "url";
         sendResponse("Background image set.");
     }
 });
