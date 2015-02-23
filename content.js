61148262ef09a37c7a0d171eb1405eb7ab8820f1
AnimalRationale <wojciech.mardyla@gmail.com>
2015-02-23 02:48:35 +0100

    Setting background color and background image.

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
     if (msg.command && (msg.command == "change_background")) {
-        document.body.style.backgroundImage = "url";
+        document.body.style.background = "#f3f3f3 url('url') no-repeat right top";
         sendResponse("Background image set.");
     }
 });
